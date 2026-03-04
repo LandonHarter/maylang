3dafea6b8a95c5d684f5b9ea0a3638346478438f
LandonHarter <landonharter@outlook.com>
2026-03-03 23:13:57 -0500

    base file reading setup

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
+
+int main(int argc, char* argv[]) {
+    FILE* fptr;
+    fptr = fopen(argv[1], "r");
+    if (fptr == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    fseek(fptr, 0L, SEEK_END);
+    unsigned long fsize = ftell(fptr);
+    rewind(fptr);
+
+    char* fbuf = (char*) malloc(fsize + 1);
+    if (fbuf == NULL) {
+        perror("Error allocating memory");
+        fclose(fptr);
+        return -1;
+    }
+
+    size_t fbuf_read = fread(fbuf, 1, fsize, fptr);
+    if (fbuf_read != fsize) {
+        perror("Error reading file");
+        free(fbuf);
+        fclose(fptr);
+        return -1;
+    }
+    fbuf[fsize] = '\0';
+
+    printf("%s", fbuf);
+
+    fclose(fptr);
+    free(fbuf);
+
+    return 0;
+}
