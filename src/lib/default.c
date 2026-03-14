6e81cf3ca826f000aa4817cda5d76dd10bd5a9fe
LandonHarter <landonharter@outlook.com>
2026-03-14 00:21:00 -0400

    assert

--- src/lib/default.c
+++ src/lib/default.c
@@ -27,11 +27,21 @@ struct MayValue* default_length(struct MayValue** args, int arg_count) {
         case MAY_STRING:
             result->as.integer = strlen(args[0]->as.string.val);
             break;
+        case MAY_ARRAY:
+            result->as.integer = args[0]->as.array.length;
+            break;
         default:
             result->as.integer = 0;
     }
 
     return result;
+}
+
+struct MayValue* default_assert(struct MayValue** args, int arg_count) {
+    if (args[0]->as.floating != 1) {
+        throw_runtime_error("Assertion failed");
+    }
+    return NULL;
 }
 
 void load_default_module(struct Env* env) {
@@ -42,4 +52,8 @@ void load_default_module(struct Env* env) {
     enum MayValueType* atlength = malloc(sizeof(enum MayValueType) * 1);
     atlength[0] = MAY_VAR;
     register_builtin(env, "length", default_length, 1, atlength);
+
+    enum MayValueType* atassert = malloc(sizeof(enum MayValueType) * 1);
+    atassert[0] = MAY_VAR;
+    register_builtin(env, "assert", default_assert, 1, atassert);
 }
